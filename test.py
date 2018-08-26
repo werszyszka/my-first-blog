3e18c355cebc164bd221539aaf9cc46cfc99d3d1
wer.szyszka <wer.szyszka@gmail.com>
2018-08-26 11:37:15 +0200

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,37 @@
+'''
+print('hello world')
+if 1<2:
+	print('Prawda')
+	name = 'Sonja'
+if name == 'Ola':
+    print('Hej Ola!')
+elif name == 'Sonja':
+    print('Hej Sonja!')
+else:
+    print('Hej anonimie!'
+def hi(imie):
+    if imie == 'Ola':
+        print('Hej Ola!')
+    elif imie == 'Sonja':
+        print('Hej Sonja!')
+    else:
+    	 print('Hej nieznajoma!')
+hi('Weronika')
+def hi(imie):
+	print('Hej '+imie+'!')
+hi('Ania')
+dziewczyny=['Ania','Ola','Monika','Beata']
+for imie in dziewczyny
+def hi(imie):
+	print ('Cześć'+imie+'!')
+for i in range(1, 6):
+	print(i)
+'''
+djangogirls
+	manage.py
+	mysite
+		settings.py
+		urls.py
+		wsgi.py
+		__init__.py
+	requirements.txt
